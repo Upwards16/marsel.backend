b15a4dc22342d0a085fd7db9905fd41a579812ea
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-02 02:56:57 +0600

    Added search tasks

---
+++ tasks/filters.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from rest_framework import filters
+from .models import Task
+
+User = get_user_model()
+
+class TaskSearchFilter(filters.SearchFilter):
+    search_param = 'search'
+
+    def filter_queryset(self, request, queryset, view):
+        search_value = request.query_params.get(self.search_param)
+
+        if search_value:
+            # Фильтрация по полям task и project__title
+            queryset = queryset.filter(
+                Q(task__icontains=search_value) |
+                Q(project__name__icontains=search_value)
+            ).distinct()
+
+        return queryset
