04c07294bc86f96cede6fea3851036b98e9b427e
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-02 02:57:37 +0600

    Added admin time sheets

--- time_sheets/admin.py
+++ time_sheets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import TimeSheet
 # Register your models here.
+
+
+@admin.register(TimeSheet)
+class TimeSheetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'task', 'date', 'time', 'comment')
+    list_filter = ('user', 'task__project', 'date')
+    search_fields = ('task__task', 'comment')
+    date_hierarchy = 'date'
