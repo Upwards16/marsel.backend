2450a3e8c5a20cd99622095f805acb8fd291f488
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-02 02:58:04 +0600

    fixed bag

--- time_sheets/serializers.py
+++ time_sheets/serializers.py
@@ -7,11 +7,17 @@ from tasks.serializers import TaskSerializer
 class TimeSheetSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False)
     task = TaskSerializer(many=False)
+    date = serializers.SerializerMethodField()
 
     class Meta:
         model = TimeSheet
         fields = '__all__'
+    def get_date(self, obj):
+        date_value = obj.date
 
+        formatted_date = date_value.strftime('%d-%m-%Y')
+
+        return formatted_date
 
 class TimeSheetCreateSerializer(serializers.ModelSerializer):
 
