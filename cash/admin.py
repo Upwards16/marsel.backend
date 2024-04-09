512ba353bafbff4a9eef912391dcf7b1160b3e1e
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-09 13:51:36 +0600

    commit

--- cash/admin.py
+++ cash/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import PaymentType, ExpenseType, Expense, Income
 # Register your models here.
+
+
+admin.site.register(PaymentType)
+admin.site.register(Expense)
+admin.site.register(ExpenseType)
+admin.site.register(Income)
