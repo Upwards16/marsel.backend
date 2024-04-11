3f7433cd49b195f4726bd97c1984c1d4e52649cf
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-12 00:41:35 +0600

    Added filtering by admin and manager for leads.

--- leads/filters.py
+++ leads/filters.py
@@ -1,15 +1,33 @@
-from django_filters import rest_framework as dj_filters
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+import django_filters as dj_filters
 from .models import Lead
 
+User = get_user_model()
 
 class LeadFilter(dj_filters.FilterSet):
     start_date = dj_filters.DateFilter(field_name="date", lookup_expr="gte")
     end_date = dj_filters.DateFilter(field_name="date", lookup_expr="lte")
+    user = dj_filters.ModelChoiceFilter(queryset=User.objects.all())
 
     class Meta:
         model = Lead
         fields = (
             'start_date',
             'end_date',
-            'status'
+            'status',
+            'user'
         )
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user and not user.is_superuser:
+            self.filters['user'] = dj_filters.ModelChoiceFilter(queryset=User.objects.filter(id=user.id))
+
+    @property
+    def qs(self):
+        parent = super().qs
+        if self.request and not self.request.user.is_superuser:
+            return parent.filter(Q(user=self.request.user) | Q(manager=self.request.user))
+        return parent
