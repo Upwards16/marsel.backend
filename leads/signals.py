37509918d25d5a03782344e58645436ea9cc35f7
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-17 17:03:57 +0600

    settings

--- leads/signals.py
+++ leads/signals.py
@@ -3,7 +3,9 @@ from django.dispatch import receiver
 from .models import Lead
 from clients.serializers import ClientCreateUpdateSerializer
 from django.db import transaction
-
+from django.utils import timezone
+from celery import shared_task
+from django.core.mail import send_mail
 
 @receiver(post_save, sender=Lead)
 def handle_lead_status_change(sender, instance, **kwargs):
@@ -26,3 +28,16 @@ def handle_lead_status_change(sender, instance, **kwargs):
             else:
                 pass
 
+
+@shared_task
+def send_reminder_email(lead_id):
+    lead = Lead.objects.get(pk=lead_id)
+    subject = 'Напоминание: {} ожидает вашего контакта'.format(lead.full_name)
+    message = 'Пожалуйста, не забудьте связаться с {} по телефону {}.'.format(lead.full_name, lead.phone)
+    send_mail(subject, message, 'from@example.com', [lead.user.email])
+
+@receiver(post_save, sender=Lead)
+def schedule_reminder_email(sender, instance, created, **kwargs):
+    if created:
+        reminder_time = instance.reminder_date - timezone.timedelta(minutes=3)
+        send_reminder_email.apply_async(args=[instance.pk], eta=reminder_time)
