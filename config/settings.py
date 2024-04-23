43c668d8b0688c481dadc67acee64d475d71331c
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-24 04:12:51 +0600

    testing

--- config/settings.py
+++ config/settings.py
@@ -233,5 +233,5 @@ SESSION_CACHE_ALIAS = "default"
 CELERY_TIMEZONE = 'Asia/Bishkek'
 
 CRONJOBS = [
-    ('56 3 * * 1-5', 'time_sheets.cron.my_scheduled_job')
+    ('20 4 * * 1-5', 'time_sheets.cron.my_scheduled_job')
 ]
--- time_sheets/cron.py
+++ time_sheets/cron.py
@@ -1,4 +1,11 @@
 from .models import TimeSheet
+from config.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
 
 def my_scheduled_job():
-    print("Hi")
+    subject = 'Test Cron'
+    message = 'Hello I\'m Upwards'
+    email_from = EMAIL_HOST_USER
+    recipient_list = ['zulpukarovmarsel17@gmail.com']
+
+    send_mail(subject, message, email_from, recipient_list)
