0c530096ff8261eb325f23888a30af0e0ecad20b
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-23 18:37:54 +0600

    update models.py

--- leads/models.py
+++ leads/models.py
@@ -43,13 +43,9 @@ class CallHistory(models.Model):
         Lead, on_delete=models.CASCADE,
         related_name="call_history"
     )
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=datetime.now())
     comment = models.TextField()
 
     def __str__(self):
         return self.lead.full_name
 
-    def save(self, *args, **kwargs):
-        if not self.date:
-            self.date = datetime.now()
-        super().save(*args, **kwargs)
