061cfbe5e36266531d42fd147a6dcaafd20d07a7
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-04-24 11:57:01 +0600

    update

--- time_sheets/cron.py
+++ time_sheets/cron.py
@@ -6,6 +6,6 @@ def my_scheduled_job():
     subject = 'Test Cron'
     message = 'Hello I\'m Upwards'
     email_from = EMAIL_HOST_USER
-    recipient_list = ['0552313636s@gmail.com']
+    recipient_list = ['zulpukarovmarsel17@gmail.com']
 
     send_mail(subject, message, email_from, recipient_list)
